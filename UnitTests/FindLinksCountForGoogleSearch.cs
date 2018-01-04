0c2e29abc67da6255dd4841ac8fb27cc4032ec54
Bhat <rbhat@dovercorp.com>
2018-01-04 18:33:41 +0530

    file name changed

---
+++ UnitTests/FindLinksCountForGoogleSearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RelevantCodes.ExtentReports;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FindLinksCountForGoogleSearch: TestBase
+    {
+        [TestMethod]
+        public void GoogleSearch()
+        {
+            extentTest = extentReport.StartTest("FindLinksCountForGoogleSearch");
+            extentTest.Log(LogStatus.Pass, "Google Page has been launched");
+            Pages.Google.EnterText(Config.SearchText);
+            extentTest.Log(LogStatus.Pass, "Entered text for search");
+            Thread.Sleep(3000);
+
+            if (Pages.SearchResults.FindLinksCount(Config.SearchText) > 0)
+                extentTest.Log(LogStatus.Pass, "Search result count is greater than 0");
+            else
+                extentTest.Log(LogStatus.Fail, "No results found");
+            
+            Assert.IsTrue(Pages.SearchResults.FindLinksCount(Config.SearchText) > 0, Config.NoResultsMessage);
+        }
+    }
+}
